773e4d71b30e7e4abaaf6a0ebb1cc90a3b1fed8f
kei-1111 <kei_02_1111@icloud.com>
2025-11-12 12:55:47 +0900

    chore: 複数のモジュールで共通して使えるプラグインを作成するbuild-logicの基礎を作成

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,25 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
+plugins {
+    `kotlin-dsl`
+}
+
+group = "io.github.kei_1111.newsflow.library.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.withType<KotlinJvmCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
+    }
+}
+
+dependencies {
+    compileOnly(libs.android.gradle)
+    compileOnly(libs.detekt.gradle)
+    compileOnly(libs.kotlin.gradle)
+}
