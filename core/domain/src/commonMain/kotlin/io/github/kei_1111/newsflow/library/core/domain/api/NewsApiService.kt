dd6a4ae0db0ff37dbfd63a912b9d1dffb7df0bdc
kei-1111 <kei_02_1111@icloud.com>
2025-11-13 23:04:55 +0900

    refactor: :core:domainに:core:networkの定義を置くことで:core:domainに依存が集中してしまうおそれがあったため削除

--- core/domain/src/commonMain/kotlin/io/github/kei_1111/newsflow/library/core/domain/api/NewsApiService.kt
+++
@@ -1,7 +0,0 @@
-package io.github.kei_1111.newsflow.library.core.domain.api
-
-import io.github.kei_1111.newsflow.library.core.model.Article
-
-interface NewsApiService {
-    suspend fun fetchTopHeadlines(category: String): List<Article>
-}
