4e27fd7e5bd32f2768ca6c46312dbec86171ed19
kei-1111 <kei_02_1111@icloud.com>
2025-11-15 15:23:57 +0900

    chore: NewsCategoryをまとめたenum classを作成

---
+++ core/model/src/commonMain/kotlin/io/github/kei_1111/newsflow/library/core/model/NewsCategory.kt
@@ -0,0 +1,11 @@
+package io.github.kei_1111.newsflow.library.core.model
+
+enum class NewsCategory(val value: String) {
+    GENERAL("general"),
+    BUSINESS("business"),
+    TECHNOLOGY("technology"),
+    ENTERTAINMENT("entertainment"),
+    SPORTS("sports"),
+    SCIENCE("science"),
+    HEALTH("health"),
+}
