f2d943731f1e326e4dd09ea36779efae3e053219
kei-1111 <kei_02_1111@icloud.com>
2025-11-22 01:03:04 +0900

    chore: 無駄なnull処理を行っていたため削除

--- core/data/src/commonMain/kotlin/io/github/kei_1111/newsflow/library/core/data/util/ResultExtension.kt
+++ core/data/src/commonMain/kotlin/io/github/kei_1111/newsflow/library/core/data/util/ResultExtension.kt
@@ -4,10 +4,10 @@ import io.github.kei_1111.newsflow.library.core.exception.NewsflowError
 import io.github.kei_1111.newsflow.library.core.network.exception.NetworkException
 
 internal fun Throwable.toNewsflowError(): NewsflowError = when (this) {
-    is NetworkException.Unauthorized -> NewsflowError.Unauthorized(message ?: "")
-    is NetworkException.RateLimitExceeded -> NewsflowError.RateLimitExceeded(message ?: "")
-    is NetworkException.BadRequest -> NewsflowError.BadRequest(message ?: "")
-    is NetworkException.ServerError -> NewsflowError.ServerError(message ?: "")
-    is NetworkException.NetworkFailure -> NewsflowError.NetworkFailure(message ?: "")
+    is NetworkException.Unauthorized -> NewsflowError.Unauthorized(message)
+    is NetworkException.RateLimitExceeded -> NewsflowError.RateLimitExceeded(message)
+    is NetworkException.BadRequest -> NewsflowError.BadRequest(message)
+    is NetworkException.ServerError -> NewsflowError.ServerError(message)
+    is NetworkException.NetworkFailure -> NewsflowError.NetworkFailure(message)
     else -> NewsflowError.NetworkFailure(message ?: "Unknown error")
 }
