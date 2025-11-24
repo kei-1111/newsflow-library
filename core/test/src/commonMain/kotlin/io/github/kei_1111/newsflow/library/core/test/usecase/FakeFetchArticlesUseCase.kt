b773975af5bed9ef47654906cd4fb88c24edef19
kei-1111 <kei_02_1111@icloud.com>
2025-11-24 21:13:16 +0900

    chore: 使っていなかったプロパティを削除

--- core/test/src/commonMain/kotlin/io/github/kei_1111/newsflow/library/core/test/usecase/FakeFetchArticlesUseCase.kt
+++ core/test/src/commonMain/kotlin/io/github/kei_1111/newsflow/library/core/test/usecase/FakeFetchArticlesUseCase.kt
@@ -7,8 +7,6 @@ class FakeFetchArticlesUseCase : FetchArticlesUseCase {
     private var result: Result<List<Article>> = Result.success(emptyList())
     var invocationCount = 0
         private set
-    var lastCategory: String = ""
-        private set
 
     fun setResult(result: Result<List<Article>>) {
         this.result = result
@@ -16,7 +14,6 @@ class FakeFetchArticlesUseCase : FetchArticlesUseCase {
 
     override suspend fun invoke(category: String): Result<List<Article>> {
         invocationCount++
-        lastCategory = category
         return result
     }
 }
