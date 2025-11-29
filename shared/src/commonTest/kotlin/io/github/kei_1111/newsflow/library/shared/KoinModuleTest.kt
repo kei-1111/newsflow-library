722579e34d4a6e047a10f73a2123d357b2931612
kei-1111 <kei_02_1111@icloud.com>
2025-11-29 12:36:44 +0900

    chore: KoinModuleTestにviewModelのテストを復元しコメントを追加するように変更

--- shared/src/commonTest/kotlin/io/github/kei_1111/newsflow/library/shared/KoinModuleTest.kt
+++ shared/src/commonTest/kotlin/io/github/kei_1111/newsflow/library/shared/KoinModuleTest.kt
@@ -7,11 +7,14 @@ import io.github.kei_1111.newsflow.library.core.domain.usecase.FetchTopHeadlineA
 import io.github.kei_1111.newsflow.library.core.domain.usecase.GetArticleByIdUseCase
 import io.github.kei_1111.newsflow.library.core.network.api.NewsApiService
 import io.github.kei_1111.newsflow.library.core.network.di.networkModule
+import io.github.kei_1111.newsflow.library.feature.home.HomeViewModel
 import io.github.kei_1111.newsflow.library.feature.home.di.homeModule
+import io.github.kei_1111.newsflow.library.feature.viewer.ViewerViewModel
 import io.github.kei_1111.newsflow.library.feature.viewer.di.viewerModule
 import io.ktor.client.HttpClient
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
+import org.koin.core.parameter.parametersOf
 import org.koin.test.KoinTest
 import org.koin.test.get
 import kotlin.test.AfterTest
@@ -68,4 +71,17 @@ class KoinModuleTest : KoinTest {
         val getArticleByIdUseCase = get<GetArticleByIdUseCase>()
         assertNotNull(getArticleByIdUseCase)
     }
+
+    /* viewModelOf()で定義したModuleはテストカバレッジ出力のためのjvmTestで失敗するためコメントアウト */
+//    @Test
+//    fun `verify HomeViewModel can be resolved`() {
+//        val homeViewModel = get<HomeViewModel>()
+//        assertNotNull(homeViewModel)
+//    }
+//
+//    @Test
+//    fun `verify ViewerViewModel can be resolved`() {
+//        val viewerViewModel = get<ViewerViewModel> { parametersOf("test-article-id") }
+//        assertNotNull(viewerViewModel)
+//    }
 }
