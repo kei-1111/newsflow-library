06bfb7bafc2db4d8f0c8062ed2ce3d9aebfce93e
kei-1111 <kei_02_1111@icloud.com>
2025-12-09 23:48:37 +0900

    ci: review-dogにコメントしてもらうように変更

--- build-logic/convention/src/main/kotlin/io/github/kei_1111/newsflow/library/Detekt.kt
+++ build-logic/convention/src/main/kotlin/io/github/kei_1111/newsflow/library/Detekt.kt
@@ -1,7 +1,10 @@
 package io.github.kei_1111.newsflow.library
 
+import io.gitlab.arturbosch.detekt.Detekt
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+import org.gradle.api.JavaVersion
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.withType
 
 internal fun Project.configureDetekt(
     extension: DetektExtension,
@@ -11,5 +14,14 @@ internal fun Project.configureDetekt(
         source.setFrom("src")
         autoCorrect = true
         parallel = true
+    }
+
+    tasks.withType<Detekt>().configureEach {
+        jvmTarget = JavaVersion.VERSION_21.toString()
+        outputs.cacheIf { true }
+        reports {
+            html.required.set(true)
+            xml.required.set(true)
+        }
     }
 }
